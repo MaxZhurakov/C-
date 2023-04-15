71ddfb4053e5ed3668fdd998d2e168cab05f60c2
MaxZhurakov <muxmax969@gmail.com>
2023-04-16 01:30:16 +0300

    commit

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+//ex_1 
+#include <iostream> 
 using namespace std;
 int main()
 {
-	cout << "Hello World!";
+    setlocale(LC_ALL, "ru");
+    cout << "\n\tДуми мої, думи мої,\n\t Лихо мені з вами!\n\tНащо стали на папері\n\tСумними рядами?..\n\t\t\tТарас Шевченко.\n\n";
+    system("pause");
+    return 0;
+}
+
+//ex_2 
+#include <iostream> 
+using namespace std;
+int main() 
+{
+    std::cout << "Every" << "\n";
+    std::cout << "\tHunter" << "\n";
+    std::cout << "\t\tWants" << "\n";
+    std::cout << "\t\t\tTo know" << "\n";
+    std::cout << "\t\t\t\t Where" << "\n";
+    std::cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+    std::cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+    return 0;
+}
+
+//ex_3 
+#include <iostream> 
+using namespace std;
+int main()
+{
+    cout << "=================================================================================\n";
+    cout << "О\tГ\tО\tЛ\tО\tШ\tЕ\tН\tН\tЯ\t!\n";
+    cout << "=================================================================================\n";
+    cout << "|\t\t\t\tПродам цуценят.\t\t\t\t\t|\n";
+    cout << "|\t\t\t\tПорода: ротвейлер.\t\t\t\t|\n";
+    cout << "=================================================================================\n";
+    cout << "|\t8\t|\t8\t|\t8\t|\t8\t|\t8\t|\n";
+    cout << "|\t\t|\t\t|\t\t|\t\t|\t\t|\n";
+    cout << "|\t920\t|\t920\t|\t920\t|\t920\t|\t920\t|\n";
+    cout << "|\t\t|\t\t|\t\t|\t\t|\t\t|\n";
+    cout << "|\t999\t|\t999\t|\t999\t|\t999\t|\t999\t|\n";
+    cout << "|\t76\t|\t76\t|\t76\t|\t76\t|\t76\t|\n";
+    cout << "|\t79\t|\t79\t|\t79\t|\t79\t|\t79\t|\n";
+    cout << "|\t\t|\t\t|\t\t|\t\t|\t\t|\n";
+    return 0;
 }

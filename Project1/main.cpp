39a0e8b3be01119dc5855f4bdbbd04ba47204389
MaxZhurakov <muxmax969@gmail.com>
2023-04-18 18:06:23 +0300

    commit

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
-using namespace std;
+//ex_1
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a;
+//	cout << "¬вед≥ть число:" ;
+//	cin >> a;
+//	for (int i = 0; i <= a; ++i)
+//		cout << i << " ";
+//	return 0;
+//}
+
+//ex_2
+#include <stdio.h>
 int main()
 {
-	cout << "Hello World!";
+	int sum = 0;
+	int input = -1;
+	while (input != 0)
+	{
+		scanf("%d", &input);
+		if ((input > 0) && (input % 2 == 0)) sum += input;
+	}
+	printf("res=%d", sum);
+	return 0;
 }
